6c78053bca231313e457501484c62fc5a01d71cc
Viktor Krapivin <viktork@localhost.localdomain>
2021-06-02 19:03:09 -0700

    added backend functionality

--- controller.py
+++ controller.py
@@ -5,6 +5,12 @@ import controller_backend as ctbe
 mainWindow = tk.Tk()
 mainWindow.title('RP100 Controller')
 
+#small tk functions
+def set_text(entryThing, text):
+    entryThing.delete(0,tk.END)
+    entryThing.insert(0,text)
+    return
+
 frame = tk.Frame(
     master=mainWindow,
     relief=tk.RAISED,
@@ -13,6 +19,74 @@ frame = tk.Frame(
 frame.grid(row=0, column=0)
 mainWindow.grid_rowconfigure(0, weight=1)
 mainWindow.grid_columnconfigure(0, weight=1)
+
+curDevice = None
+device1Enabled = False
+device2Enabled = False
+msUpdateSpeed = 1000
+# graphics methods
+def updateVolts():
+    # curVolt1= curDevice.getSetVoltage()
+    curVolt1= curDevice.measVoltage()
+    print(curVolt1)
+    set_text(source1CurVoltage,str(curVolt1))
+    # curVolt2= curDevice.getSetVoltage(deviceNum=2)
+    curVolt2= curDevice.measVoltage(deviceNum=2)
+    print(curVolt2)
+    set_text(source2CurVoltage,str(curVolt2))
+    mainWindow.after(msUpdateSpeed, updateVolts)
+def updateSlew():
+    curSlew1= curDevice.getSetSlew()
+    set_text(source1CurSlew,str(curSlew1))
+    curSlew2= curDevice.getSetSlew(deviceNum=2)
+    set_text(source2CurSlew,str(curSlew2))
+    mainWindow.after(msUpdateSpeed, updateSlew)
+def openNewDevice(*args):
+    print(chosenDevice.get())
+    print('opening new device above.')
+    curDevice = ctbe.rp100(chosenDevice.get())
+def toggleDevice1():
+    global device1Enabled
+    if device1Enabled == False:
+        curDevice.enableDevice()
+        device1Enabled = True
+    else:
+        curDevice.disableDevice()
+        device1Enabled = False
+def toggleDevice2():
+    global device2Enabled
+    if device2Enabled == False:
+        curDevice.enableDevice(deviceNum=2)
+        device2Enabled = True
+    else:
+        curDevice.disableDevice(deviceNum=2)
+        device2Enabled = False
+def addFiveVoltsDevice1():
+    curSetVolt = curDevice.getSetVoltage()
+    curDevice.setVoltage(numVolts=curSetVolt + 5.0)
+def subtractFiveVoltsDevice1():
+    curSetVolt = curDevice.getSetVoltage()
+    curDevice.setVoltage(numVolts = curSetVolt - 5.0)
+def addFiveVoltsDevice2():
+    curSetVolt = curDevice.getSetVoltage(deviceNum=2)
+    curDevice.setVoltage(deviceNum=2,numVolts = curSetVolt + 5.0)
+def subtractFiveVoltsDevice2():
+    curSetVolt = curDevice.getSetVoltage(deviceNum=2)
+    curDevice.setVoltage(deviceNum=2,numVolts = curSetVolt - 5.0)
+def multiplySlewBy10Device1():
+    curSetSlew = curDevice.getSetSlew()
+    curDevice.setSlew(numSlew=curSetSlew * 10.0)
+def divideSlewBy10Device1():
+    curSetSlew = curDevice.getSetSlew()
+    curDevice.setSlew(numSlew = curSetSlew / 10.0)
+def multiplySlewBy10Device2():
+    curSetSlew = curDevice.getSetSlew(deviceNum=2)
+    curDevice.setSlew(deviceNum=2,numSlew = curSetSlew* 10.0)
+def divideSlewBy10Device2():
+    curSetSlew = curDevice.getSetSlew(deviceNum=2)
+    curDevice.setSlew(deviceNum=2,numSlew = curSetSlew / 10.0)
+
+# source 1
 source1Label = tk.Label(master=frame, text='Source 1')
 source1Label.grid(row=0, column=0, columnspan=3)
 source1StatusChangeButton = tk.Button(
@@ -22,6 +96,7 @@ source1StatusChangeButton = tk.Button(
     height=5,
     bg="white",
     fg="black",
+    command = toggleDevice1,
 )
 source1StatusChangeButton.grid(row=1, column=0, columnspan=3)
 # voltage controls
@@ -32,6 +107,7 @@ source1LessVoltage = tk.Button(
     height=5,
     bg="white",
     fg="black",
+    command = subtractFiveVoltsDevice1,
 )
 source1LessVoltage.grid(row=2, column=0)
 source1CurVoltage = tk.Entry(
@@ -48,16 +124,18 @@ source1MoreVoltage = tk.Button(
     height=5,
     bg="white",
     fg="black",
+    command = addFiveVoltsDevice1,
 )
 source1MoreVoltage.grid(row=2, column=2)
 # source 1 voltage slew controls
 source1LessSlew = tk.Button(
     master=frame, 
-    text="Minus 1 V/s",
+    text="Divide Slew by 10",
     width=8,
     height=5,
     bg="white",
     fg="black",
+    command = divideSlewBy10Device1,
 )
 source1LessSlew.grid(row=3, column=0)
 source1CurSlew = tk.Entry(
@@ -68,11 +146,12 @@ source1CurSlew = tk.Entry(
 source1CurSlew.grid(row=3, column=1)
 source1MoreSlew = tk.Button(
     master=frame, 
-    text="Plus 1 V/s",
+    text="Multiply Slew by 10",
     width=8,
     height=5,
     bg="white",
     fg="black",
+    command = multiplySlewBy10Device1,
 )
 source1MoreSlew.grid(row=3, column=2)
 
@@ -86,6 +165,7 @@ source2StatusChangeButton = tk.Button(
     height=5,
     bg="white",
     fg="black",
+    command = toggleDevice2,
 )
 source2StatusChangeButton.grid(row=1, column=3, columnspan=3)
 source2LessVoltage = tk.Button(
@@ -95,6 +175,7 @@ source2LessVoltage = tk.Button(
     height=5,
     bg="white",
     fg="black",
+    command = subtractFiveVoltsDevice2,
 )
 source2LessVoltage.grid(row=2, column=3)
 source2CurVoltage = tk.Entry(
@@ -110,16 +191,18 @@ source2MoreVoltage = tk.Button(
     height=5,
     bg="white",
     fg="black",
+    command = addFiveVoltsDevice2,
 )
 source2MoreVoltage.grid(row=2, column=5)
 # source 2 voltage slew controls
 source2LessSlew = tk.Button(
     master=frame, 
-    text="Minus 1 V/s",
+    text="Divide by 10",
     width=8,
     height=5,
     bg="white",
     fg="black",
+    command = divideSlewBy10Device2,
 )
 source2LessSlew.grid(row=3, column=3)
 source2CurSlew = tk.Entry(
@@ -130,19 +213,25 @@ source2CurSlew = tk.Entry(
 source2CurSlew.grid(row=3, column=4)
 source2MoreSlew = tk.Button(
     master=frame, 
-    text="Plus 1 V/s",
+    text="Multiply by 10",
     width=8,
     height=5,
     bg="white",
     fg="black",
+    command = multiplySlewBy10Device2,
 )
 source2MoreSlew.grid(row=3, column=5)
-def printCur(*args):
-    print(chosenDevice.get())
+
+
+
 comDevices = tuple(ctbe.listPossibleSerials())
 chosenDevice = tk.StringVar()
 chosenDevice.set(comDevices[-1])
-deviceDropdown = tk.OptionMenu(frame, chosenDevice, *comDevices, command = printCur)
+curDevice = ctbe.rp100(chosenDevice.get())
+deviceDropdown = tk.OptionMenu(frame, chosenDevice, *comDevices, command = openNewDevice)
 deviceDropdown.grid(row=4,columnspan=6)
+
+mainWindow.after(msUpdateSpeed, updateVolts)
+mainWindow.after(msUpdateSpeed, updateSlew)
 #start window
 mainWindow.mainloop()
--- controller_backend.py
+++ controller_backend.py
@@ -12,9 +12,11 @@ def listPossibleSerials():
     )
     possibleDevices = []
     for deviceString in findPossibleConnections.stdout.splitlines():
-        if re.search('^ttyS', deviceString):
-            possibleDevices.append(deviceString)
+        # if re.search('^ttyS', deviceString):
+        #     possibleDevices.append(deviceString)
         if re.search('^ttyUSB', deviceString):
+            possibleDevices.append(deviceString)
+        if re.search('^ttyACM', deviceString):
             possibleDevices.append(deviceString)
     possibleDevices = tuple(possibleDevices)
     return possibleDevices
@@ -22,7 +24,65 @@ def listPossibleSerials():
 # print(listPossibleSerials())
 
 
-
+class rp100:
+    serialConnection = None
+    deviceID = None
+    def writeBytes(self,bytesToBeWritten):
+        # get rid of anything incoming from the device before writing
+        # self.serialConnection.reset_input_buffer()
+        print(bytesToBeWritten)
+        self.serialConnection.write(bytesToBeWritten)
+    def readBytes(self,numToRead = 100):
+        self._readBytes = self.serialConnection.read(numToRead)
+        print(self._readBytes)
+        return self._readBytes
+    def resetDevice(self):
+        self.writeBytes(b'*RST\r\n')
+    def clearErrs(self):
+        self.writeBytes(b'*CLS\r\n')
+    def getSetVoltage(self,deviceNum=1):
+        _writeBytes = b'SOUR'+bytes(str(deviceNum),'utf-8')+b':VOLT?\r\n'
+        self.writeBytes(_writeBytes)
+        self._sVolt = self.readBytes()
+        return float(self._sVolt)
+    def getCurSetVoltage(self,deviceNum=1):
+        _writeBytes = b'SOUR'+bytes(str(deviceNum),'utf-8')+b':VOLT:NOW?\r\n'
+        self.writeBytes(_writeBytes)
+        self._sVolt = self.serialConnection.read(100)
+        return float(self._sVolt)
+    def measVoltage(self,deviceNum=1):
+        _writeBytes = b'MEAS'+bytes(str(deviceNum),'utf-8')+b':VOLT?\r\n'
+        self.writeBytes(_writeBytes)
+        self._mVolt = self.readBytes()
+        return float(self._mVolt)
+    def getSetSlew(self,deviceNum=1):
+        _writeBytes = b'SOUR'+bytes(str(deviceNum),'utf-8')+b':VOLT:SLEW?\r\n'
+        self.writeBytes(_writeBytes)
+        self._sSlew = self.readBytes()
+        return float(self._sSlew)
+    def setVoltage(self,deviceNum=1,numVolts = 0.0):
+        _writeBytes = b'SOUR'+bytes(str(deviceNum),'utf-8')+b':VOLT ' + bytes(str(numVolts),'utf-8') + b'\r\n'
+        self.writeBytes(_writeBytes)
+    def setSlew(self,deviceNum=1,numSlew = 0.0):
+        _writeBytes = b'SOUR'+bytes(str(deviceNum),'utf-8')+b':VOLT:SLEW ' + bytes(str(numSlew),'utf-8') + b'\r\n'
+        self.writeBytes(_writeBytes)
+    def enableDevice(self,deviceNum=1):
+        _writeBytes = b'OUTP'+bytes(str(deviceNum),'utf-8') + b' 1\r\n'
+        self.writeBytes(_writeBytes)
+    def disableDevice(self,deviceNum=1):
+        _writeBytes = b'OUTP'+bytes(str(deviceNum),'utf-8') + b' 0\r\n'
+        self.writeBytes(_writeBytes)
+        
+    def __init__(self, serialPort, prependDev=True):
+        openUSBPort = serialPort
+        if prependDev is True:
+            openUSBPort = '/dev/' + openUSBPort
+        self.serialConnection = srl.Serial(openUSBPort,timeout=0.05)
+        # clear any errors, reset device to just turned on.
+        self.clearErrs()
+        self.resetDevice()
+        self.writeBytes(b'*IDN?\r\n')
+        self.deviceID = self.readBytes().decode('utf-8')
 
 
 
